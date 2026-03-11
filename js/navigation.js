0e28341f623c673c673f2efd7b6c72a8e6c04787
RANDRIA Luca <lucarandrianirina2507@gmail.com>
2026-03-11 18:24:45 +0300

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -11,27 +11,4 @@ document.addEventListener('DOMContentLoaded', function() {
       item.classList.remove('active');
     }
   });
-
-  // Application du thème au chargement 
-  function applyGlobalTheme() {
-    const isDark = localStorage.getItem('sotroyDarkTheme') === 'true';
-
-    if (isDark) {
-       document.documentElement.style.setProperty('--white', '#2c3e50');
-       document.documentElement.style.setProperty('--light-gray', '#34495e');
-       document.documentElement.style.setProperty('--text', '#ecf0f1');
-       document.documentElement.style.setProperty('--dark-gray', '#bdc3c7');
-       document.documentElement.style.setProperty('--medium-gray', '#4a6572');
-    } else {
-      document.documentElement.style.setProperty('--white', '#ffffff');
-      document.documentElement.style.setProperty('--light-gray', '#f8f9fa');
-      document.documentElement.style.setProperty('--text', '#2c3e50');
-      document.documentElement.style.setProperty('--dark-gray', '#343a40');
-      document.documentElement.style.setProperty('--medium-gray', '#e9ecef');
-    }
-  }
-
-  applyGlobalTheme();
-
-  window.addEventListener('settingsChanged', applyGlobalTheme);
 });
